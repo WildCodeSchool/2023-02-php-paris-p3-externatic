9145f07a535011e368a8bede8a7b6cb45fbdb46c
LEA HADIDA <hadida.lea@gmail.com>
2023-06-22 00:12:11 +0200

    :construction_worker: added mixin for cards

--- assets/app.js
+++ assets/app.js
@@ -1,5 +1,20 @@
+
 import './styles/app.scss';
 
 import './bootstrap';
 
 require('bootstrap');
+
+
+let expandedPromo = false;
+
+function showCheckboxesPromo() {
+    let checkboxesPromo = document.getElementById("checkboxesPromo");
+    if (!expandedPromo) {
+        checkboxesPromo.style.display = "block";
+        expandedPromo = true;
+    } else {
+        checkboxesPromo.style.display = "none";
+        expandedPromo = false;
+    }
+}
