33c3fb5a649950ca04e473670d536500825e06e6
LEA HADIDA <hadida.lea@gmail.com>
2023-06-22 20:16:25 +0200

    :construction_worker: added type of job dropdown menu and nagigation buttons

--- assets/app.js
+++ assets/app.js
@@ -4,17 +4,3 @@ import './styles/app.scss';
 import './bootstrap';
 
 require('bootstrap');
-
-
-let expandedPromo = false;
-
-function showCheckboxesPromo() {
-    let checkboxesPromo = document.getElementById("checkboxesPromo");
-    if (!expandedPromo) {
-        checkboxesPromo.style.display = "block";
-        expandedPromo = true;
-    } else {
-        checkboxesPromo.style.display = "none";
-        expandedPromo = false;
-    }
-}
