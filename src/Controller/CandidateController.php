a986c4c9b3efc90223e7416bfc0828d703953e30
Esther Martinez <estmartinezl@gmail.com>
2023-06-22 11:01:06 +0200

    :construction_worker: added information in candidate/show

---
+++ src/Controller/CandidateController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Candidate;
+use App\Form\CandidateType;
+use App\Repository\CandidateRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/candidate')]
+class CandidateController extends AbstractController
+{
+    #[Route('/', name: 'candidate_index', methods: ['GET'])]
+    public function index(CandidateRepository $candidateRepository): Response
+    {
+        return $this->render('candidate/index.html.twig', [
+            'candidates' => $candidateRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/new', name: 'candidate_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, CandidateRepository $candidateRepository): Response
+    {
+        $candidate = new Candidate();
+        $form = $this->createForm(CandidateType::class, $candidate);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $candidateRepository->save($candidate, true);
+
+            return $this->redirectToRoute('candidate_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('candidate/new.html.twig', [
+            'candidate' => $candidate,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'candidate_show', methods: ['GET'])]
+    public function show(Candidate $candidate): Response
+    {
+        return $this->render('candidate/show.html.twig', [
+            'candidate' => $candidate,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'candidate_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Candidate $candidate, CandidateRepository $candidateRepository): Response
+    {
+        $form = $this->createForm(CandidateType::class, $candidate);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $candidateRepository->save($candidate, true);
+
+            return $this->redirectToRoute('candidate_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('candidate/edit.html.twig', [
+            'candidate' => $candidate,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'candidate_delete', methods: ['POST'])]
+    public function delete(Request $request, Candidate $candidate, CandidateRepository $candidateRepository): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$candidate->getId(), $request->request->get('_token'))) {
+            $candidateRepository->remove($candidate, true);
+        }
+
+        return $this->redirectToRoute('candidate_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
