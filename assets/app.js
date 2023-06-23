6b3b7e89033258643735bf1374a7b1f14f5f66a6
Esther Martinez <estmartinezl@gmail.com>
2023-06-23 16:39:15 +0200

    :construction_worker: added button switch visibility

--- assets/app.js
+++ assets/app.js
@@ -1,5 +1,5 @@
 import './styles/app.scss';
-
 import './bootstrap';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
 require('bootstrap');
--- src/Controller/CandidateController.php
+++ src/Controller/CandidateController.php
@@ -5,6 +5,8 @@ namespace App\Controller;
 use App\Entity\Candidate;
 use App\Form\CandidateType;
 use App\Repository\CandidateRepository;
+use App\Service\Visibility;
+use Doctrine\DBAL\Schema\Visitor\Visitor;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -48,6 +50,21 @@ class CandidateController extends AbstractController
         ]);
     }
 
+    #[Route('/{id}/changeVisibility', name: 'candidate_visibility')]
+    public function changeVisibility(CandidateRepository $candidateRepository, Candidate $candidate): Response
+    {
+        $candidate = $candidateRepository->findOneById($candidate->getId());
+
+        if ($candidate->isVisible()) {
+            $candidate->setVisible(false);
+        } else {
+            $candidate->setVisible(true);
+        }
+        $candidateRepository->save($candidate, true);
+
+        return $this->redirectToRoute('candidate_show', ['id' => $candidate->getId()], Response::HTTP_SEE_OTHER);
+    }
+
     #[Route('/{id}/edit', name: 'candidate_edit', methods: ['GET', 'POST'])]
     public function edit(Request $request, Candidate $candidate, CandidateRepository $candidateRepository): Response
     {
@@ -69,7 +86,7 @@ class CandidateController extends AbstractController
     #[Route('/{id}', name: 'candidate_delete', methods: ['POST'])]
     public function delete(Request $request, Candidate $candidate, CandidateRepository $candidateRepository): Response
     {
-        if ($this->isCsrfTokenValid('delete'.$candidate->getId(), $request->request->get('_token'))) {
+        if ($this->isCsrfTokenValid('delete' . $candidate->getId(), $request->request->get('_token'))) {
             $candidateRepository->remove($candidate, true);
         }
 
