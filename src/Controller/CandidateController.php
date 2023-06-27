f82855135542d25c4dcd42e5f2d8463ab68b1c78
Esther Martinez <estmartinezl@gmail.com>
2023-06-27 22:00:55 +0200

    ✨ fixed comments

--- src/Controller/CandidateController.php
+++ src/Controller/CandidateController.php
@@ -12,10 +12,10 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/candidate')]
+#[Route('/candidate', name: 'candidate_')]
 class CandidateController extends AbstractController
 {
-    #[Route('/', name: 'candidate_index', methods: ['GET'])]
+    #[Route('/', name: 'index', methods: ['GET'])]
     public function index(CandidateRepository $candidateRepository): Response
     {
         return $this->render('candidate/index.html.twig', [
@@ -23,7 +23,7 @@ class CandidateController extends AbstractController
         ]);
     }
 
-    #[Route('/new', name: 'candidate_new', methods: ['GET', 'POST'])]
+    #[Route('/new', name: 'new', methods: ['GET', 'POST'])]
     public function new(Request $request, CandidateRepository $candidateRepository): Response
     {
         $candidate = new Candidate();
@@ -42,7 +42,7 @@ class CandidateController extends AbstractController
         ]);
     }
 
-    #[Route('/{id}', name: 'candidate_show', methods: ['GET'])]
+    #[Route('/{id}', name: 'show', methods: ['GET'])]
     public function show(Candidate $candidate): Response
     {
         return $this->render('candidate/show.html.twig', [
@@ -50,7 +50,7 @@ class CandidateController extends AbstractController
         ]);
     }
 
-    #[Route('/{id}/changeVisibility', name: 'candidate_visibility')]
+    #[Route('/{id}/changeVisibility', name: 'visibility')]
     public function changeVisibility(CandidateRepository $candidateRepository, Candidate $candidate): Response
     {
         $candidate = $candidateRepository->findOneById($candidate->getId());
@@ -65,7 +65,7 @@ class CandidateController extends AbstractController
         return $this->redirectToRoute('candidate_show', ['id' => $candidate->getId()], Response::HTTP_SEE_OTHER);
     }
 
-    #[Route('/{id}/edit', name: 'candidate_edit', methods: ['GET', 'POST'])]
+    #[Route('/{id}/edit', name: 'edit', methods: ['GET', 'POST'])]
     public function edit(Request $request, Candidate $candidate, CandidateRepository $candidateRepository): Response
     {
         $form = $this->createForm(CandidateType::class, $candidate);
@@ -83,7 +83,7 @@ class CandidateController extends AbstractController
         ]);
     }
 
-    #[Route('/{id}', name: 'candidate_delete', methods: ['POST'])]
+    #[Route('/{id}', name: 'delete', methods: ['POST'])]
     public function delete(Request $request, Candidate $candidate, CandidateRepository $candidateRepository): Response
     {
         if ($this->isCsrfTokenValid('delete' . $candidate->getId(), $request->request->get('_token'))) {
