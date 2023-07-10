2cda712158e3039dc5fd776ffdd0f0ec03cc9e2e
Erika <erika.ike@outlook.fr>
2023-07-10 09:56:14 +0200

    vich

--- src/Entity/Offer.php
+++ src/Entity/Offer.php
@@ -9,7 +9,6 @@ use Doctrine\ORM\Mapping as ORM;
 use App\Repository\OfferRepository;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\Common\Collections\ArrayCollection;
-// use Vich\UploaderBundle\Entity\File;
 use Vich\UploaderBundle\Mapping\Annotation as Vich;
 use Symfony\Component\Validator\Constraints as Assert;
 use Symfony\Component\HttpFoundation\File\File;
