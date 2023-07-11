5cbc597dd1bc2a24c403d88e3086b1709c410582
Erika <erika.ike@outlook.fr>
2023-07-11 15:36:02 +0200

    after Ester's relecture

--- src/Controller/OfferTypeController.php
+++ src/Controller/OfferTypeController.php
@@ -16,17 +16,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 #[Route('/offer/form', name:'offer_form_')]
 class OfferTypeController extends AbstractController
 {
-    #[Route('/', name: 'app_offer_type_index', methods: ['GET'])]
-    public function index(OfferRepository $offerRepository): Response
-    {
-        return $this->render('offer_type/index.html.twig', [
-            'offers' => $offerRepository->findAll(),
-        ]);
-    }
-
-
     #[Route('/new', name: 'new', methods: ['GET', 'POST'])]
-    public function new(Request $request, EntityManagerInterface $manager): Response
+    public function new(Request $request, OfferRepository $repo): Response
     {
         $offer = new Offer();
         $form = $this->createForm(OfferType::class, $offer);
@@ -36,8 +27,7 @@ class OfferTypeController extends AbstractController
             $offer = $form->getData();
             $offer->setCompany($this->getUser()->getCompany());
 
-            $manager->persist($offer);
-            $manager->flush();
+            $repo->save($offer, true);
 
             $this->addFlash(
                 'succes',
@@ -53,15 +43,7 @@ class OfferTypeController extends AbstractController
         ]);
     }
 
-    #[Route('/{id}', name: 'app_offer_type_show', methods: ['GET'])]
-    public function show(Offer $offer): Response
-    {
-        return $this->render('offer_type/show.html.twig', [
-            'offer' => $offer,
-        ]);
-    }
-
-    #[Route('/{id}/edit', name: 'app_offer_type_edit', methods: ['GET', 'POST'])]
+    #[Route('/{id}/edit', name: 'edit', methods: ['GET', 'POST'])]
     public function edit(Request $request, Offer $offer, OfferRepository $offerRepository): Response
     {
         $form = $this->createForm(OfferType::class, $offer);
@@ -73,13 +55,13 @@ class OfferTypeController extends AbstractController
             return $this->redirectToRoute('app_offer_type_index', [], Response::HTTP_SEE_OTHER);
         }
 
-        return $this->renderForm('offer_type/edit.html.twig', [
+        return $this->render('offer_type/edit.html.twig', [
             'offer' => $offer,
             'form' => $form,
         ]);
     }
 
-    #[Route('/{id}', name: 'app_offer_type_delete', methods: ['POST'])]
+    #[Route('/{id}', name: 'delete', methods: ['POST'])]
     public function delete(Request $request, Offer $offer, OfferRepository $offerRepository): Response
     {
         if ($this->isCsrfTokenValid('delete' . $offer->getId(), $request->request->get('_token'))) {
