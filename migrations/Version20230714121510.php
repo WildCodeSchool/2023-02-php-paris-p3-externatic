86af0b824c467c6f137a79563833ce046a559958
Da Rosa Lionel <lioneldarosa@gmail.com>
2023-07-14 16:12:35 +0200

    :poop: fix show applications

--- migrations/Version20230714121510.php
+++ migrations/Version20230714121510.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230713083517 extends AbstractMigration
+final class Version20230714121510 extends AbstractMigration
 {
     public function getDescription(): string
     {
