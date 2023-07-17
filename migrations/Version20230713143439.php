0b89dd3e19a9485dafdb027375213f4e8778ba9d
Da Rosa Lionel <lioneldarosa@gmail.com>
2023-07-17 09:23:17 +0200

    some fixes

--- migrations/Version20230713143439.php
+++ migrations/Version20230713143439.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230713083517 extends AbstractMigration
+final class Version20230713143439 extends AbstractMigration
 {
     public function getDescription(): string
     {
