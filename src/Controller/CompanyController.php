e9c9e7f37b68b0a674562d526e87b392f53045ad
Da Rosa Lionel <lioneldarosa@gmail.com>
2023-07-18 11:45:15 +0200

    add company application candidate page

--- src/Controller/CompanyController.php
+++ src/Controller/CompanyController.php
@@ -2,9 +2,14 @@
 
 namespace App\Controller;
 
+use App\Entity\Application;
+use App\Entity\Candidate;
 use App\Entity\Company;
+use App\Form\ApplicationStatusType;
+use App\Repository\ApplicationRepository;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -17,6 +22,29 @@ class CompanyController extends AbstractController
     {
         return $this->render('company/indexOffer.html.twig', [
             'company' => $company,
+        ]);
+    }
+
+    #[Route('/candidate/application/{id}', name: 'candidate_application')]
+    public function candidateApplication(
+        Application $application,
+        Request $request,
+        ApplicationRepository $repository
+        ): Response {
+        $form = $this->createForm(ApplicationStatusType::class, $application);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $repository->save($application, true);
+
+            return $this->redirectToRoute('company_offers', [
+                'id' => $application->getOffer()->getCompany()->getId()
+            ], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('company/candidateApplication.html.twig', [
+            'application' => $application,
+            'form' => $form,
         ]);
     }
 }
